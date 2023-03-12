6ccba2a8b8357cce4255e26d6598e1620156bdaa
natlya_voevodina <voevodochka1988@yandex.ru>
2023-03-12 13:29:23 +0300

    task2.8

---
+++ task2.8/Program.cs
@@ -0,0 +1,54 @@
+﻿void Fillarray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);
+}
+
+void Printarray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+
+void SumStringArray(int[,] array)// минимальная сумма элементов в строке
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty; // пустая строка
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        line += array[index, j] + " "; 
+    }
+    Console.WriteLine($"Строка с минимальной суммой элементов -> {index+1}");
+}
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+Fillarray(array);
+Printarray(array);
+Console.WriteLine();
+SumStringArray(array);
